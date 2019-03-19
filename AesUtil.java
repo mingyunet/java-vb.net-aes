cc269a5ba8728b7fa7f97ae06c683ff434246786
TZ0528 <i-net@qq.com>
2019-03-19 15:08:53 +0800

    init

---
+++ AesUtil.java
@@ -0,0 +1,38 @@
+﻿package com.pmcc.test;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+
+public class AesUtil {
+	public static String aesEncrypt(String str, String key) throws Exception {
+		if (str == null || key == null)
+			return null;
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes("utf-8"), "AES"));
+		byte[] bytes = cipher.doFinal(str.getBytes("utf-8"));
+		return new BASE64Encoder().encode(bytes);
+	}
+
+	public static String aesDecrypt(String str, String key) throws Exception {
+		if (str == null || key == null)
+			return null;
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes("utf-8"), "AES"));
+		byte[] bytes = new BASE64Decoder().decodeBuffer(str);
+		bytes = cipher.doFinal(bytes);
+		return new String(bytes, "utf-8");
+	}
+
+	public static void main(String[] args) throws Exception {
+		String s = "hello world 2019";
+		String key = "qwer1234asdf5678";
+		System.out.println("秘钥前:" + key);
+		System.out.println("加密前:" + s);
+		String s1 = aesEncrypt(s, key);
+		System.out.println("加密后:" + s1);
+		System.out.println("解密后:" + aesDecrypt(s1, key));
+	}
+}
---
+++ AesUtil.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Text
+
+Public Class AesUtil
+    'AES加密
+    Public Function AES_Encrypt(ByVal input As String, ByVal pass As String) As String
+        If input.Trim().Length() = 0 Then
+            Return ""
+        End If
+
+        Dim toEncryptArray() As Byte = Encoding.UTF8.GetBytes(input)
+
+        Dim rm As New System.Security.Cryptography.RijndaelManaged
+        rm.Key = Encoding.UTF8.GetBytes(pass)
+        rm.Mode = Security.Cryptography.CipherMode.ECB
+        rm.Padding = Security.Cryptography.PaddingMode.PKCS7
+
+        Dim cTransform As System.Security.Cryptography.ICryptoTransform = rm.CreateEncryptor
+        Dim resultArray() As Byte = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length)
+
+        Return Convert.ToBase64String(resultArray, 0, resultArray.Length)
+    End Function
+
+
+    'AES解密
+    Public Function AES_Decrypt(ByVal input As String, ByVal pass As String) As String
+        If input.Trim().Length() = 0 Then
+            Return ""
+        End If
+        Dim toEncryptArray() As Byte = Convert.FromBase64String(input)
+        Dim rm As New System.Security.Cryptography.RijndaelManaged
+        rm.Key = Encoding.UTF8.GetBytes(pass)
+        rm.Mode = Security.Cryptography.CipherMode.ECB
+        rm.Padding = Security.Cryptography.PaddingMode.PKCS7
+        Dim cTransform As System.Security.Cryptography.ICryptoTransform = rm.CreateDecryptor
+        Dim resultArray() As Byte = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length)
+        Return Encoding.UTF8.GetString(resultArray)
+    End Function
+
+End Class
